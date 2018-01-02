414704b66b6ed1a8b6bf9e977134579064bfc031
AScislawska <aleksandra.scislawska@gmail.com>
2018-01-02 22:22:02 +0100

    new update

--- src/org/ws/communication/RequestQuizesMessage.java
+++ src/org/ws/communication/RequestQuizesMessage.java
@@ -1,6 +1,5 @@
 package org.ws.communication;
 
-import org.ws.communication.SocketMessage;
 
 import java.io.Serializable;
 import java.util.List;
@@ -9,9 +8,9 @@ public class RequestQuizesMessage extends SocketMessage implements Serializable 
 
     private static final long serialVersionUID = -6532683589417248936L;
 
-    private List<Long> quizes;
+    private List<String> quizes;
 
-    public RequestQuizesMessage(List<Long> quizes, String requesterName){
+    public RequestQuizesMessage(List<String> quizes, String requesterName){
         super(requesterName);
         this.quizes = quizes;
     }
@@ -19,11 +18,11 @@ public class RequestQuizesMessage extends SocketMessage implements Serializable 
     @Deprecated
     public RequestQuizesMessage(){    }
 
-    public List<Long> getQuizes() {
+    public List<String> getQuizes() {
         return quizes;
     }
 
-    public void setQuizes(List<Long> quizes) {
+    public void setQuizes(List<String> quizes) {
         this.quizes = quizes;
     }
 
